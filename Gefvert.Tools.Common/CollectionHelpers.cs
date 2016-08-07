b443ead00a7cbe7dd955d12173f57c5a9c67838c
Mats Gefvert <mats@gefvert.org>
2016-08-07 14:18:32 -0400

    ExtractFirstOrDefault etc.

--- Gefvert.Tools.Common/CollectionHelpers.cs
+++ Gefvert.Tools.Common/CollectionHelpers.cs
@@ -33,6 +33,11 @@ namespace Gefvert.Tools.Common
       return result;
     }
 
+    public static T ExtractAtOrDefault<T>(this IList<T> list, int position)
+    {
+      return position < 0 || position >= list.Count ? default(T) : ExtractAt(list, position);
+    }
+
     public static List<T> ExtractAll<T>(this IList<T> list, Predicate<T> match)
     {
       var result = list.Where(x => match(x)).ToList();
@@ -47,9 +52,19 @@ namespace Gefvert.Tools.Common
       return ExtractAt(list, 0);
     }
 
+    public static T ExtractFirstOrDefault<T>(this IList<T> list)
+    {
+      return list.Any() ? ExtractAt(list, 0) : default(T);
+    }
+
     public static T ExtractLast<T>(this IList<T> list)
     {
       return ExtractAt(list, list.Count - 1);
+    }
+
+    public static T ExtractLastOrDefault<T>(this IList<T> list)
+    {
+      return list.Any() ? ExtractAt(list, list.Count - 1) : default(T);
     }
 
     public static TValue GetOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
@@ -70,6 +85,18 @@ namespace Gefvert.Tools.Common
       value += GetOrDefault(dictionary, key);
       dictionary[key] = value;
       return value;
+    }
+
+    public static bool Swap<T>(this IList<T> list, int pos1, int pos2)
+    {
+      if (pos1 < 0 || pos2 < 0 || pos1 >= list.Count || pos2 >= list.Count)
+        return false;
+
+      var item = list[pos1];
+      list[pos1] = list[pos2];
+      list[pos2] = item;
+
+      return true;
     }
   }
 }
