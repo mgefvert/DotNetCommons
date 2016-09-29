82a0c7034d542e406b062cb0b09c17ed7d377618
Mats Gefvert <mats@gefvert.org>
2016-09-29 12:03:48 -0400

    Added logging; passwords

---
+++ CommonNetTools/_Logger/ILogFileNaming.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CommonNetTools
+{
+    internal interface ILogFileNaming
+    {
+        IEnumerable<string> GetAllowedFiles(string name, string extension, int rotations, DateTime? date = null);
+        string GetCurrentFileName(string name, string extension, DateTime? date = null);
+        string GetFileSpec(string name, string extension);
+    }
+}
