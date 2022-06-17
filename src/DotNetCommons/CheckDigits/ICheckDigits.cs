021177d400dcc69064031bb60118b76c48eb468a
Mats Gefvert <mats@gefvert.org>
2022-06-17 00:32:24 -0400

    Add CheckDigits

--- src/DotNetCommons/CheckDigits/ICheckDigits.cs
+++ src/DotNetCommons/CheckDigits/ICheckDigits.cs
@@ -1,23 +1,22 @@
-﻿namespace DotNetCommons.CheckDigits
+﻿namespace DotNetCommons.CheckDigits;
+
+/// <summary>
+/// Check digit calculations.
+/// </summary>
+public interface ICheckDigits
 {
     /// <summary>
-    /// Check digit calculations.
+    /// Calculate a new check digit from a given input string.
     /// </summary>
-    public interface ICheckDigits
-    {
-        /// <summary>
-        /// Calculate a new check digit from a given input string.
-        /// </summary>
-        char Calculate(string input);
+    char Calculate(string input);
 
-        /// <summary>
-        /// Append a new check digit to a given input string.
-        /// </summary>
-        string Append(string input) => input + Calculate(input);
+    /// <summary>
+    /// Append a new check digit to a given input string.
+    /// </summary>
+    string Append(string input) => input + Calculate(input);
 
-        /// <summary>
-        /// Validate whether the input string has a correct check digit or not.
-        /// </summary>
-        bool Validate(string input) => input == Append(input[0..^1]);
-    }
+    /// <summary>
+    /// Validate whether the input string has a correct check digit or not.
+    /// </summary>
+    bool Validate(string input) => input == Append(input[0..^1]);
 }
