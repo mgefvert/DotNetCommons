8f9a865865c99f318e9992b5c97c73a80d0063f5
Mats Gefvert <mats@gefvert.org>
2024-11-05 15:43:58 -0500

    Color models

---
+++ src/DotNetCommons/Colors/GrayscaleColor.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+
+namespace DotNetCommons.Colors;
+
+public class GrayscaleColor
+{
+    private double _value;
+    private double _alpha;
+
+    public double Value
+    {
+        get => _value;
+        set => _value = Math.Clamp(value, 0, 255);
+    }
+
+    public double Alpha
+    {
+        get => _alpha;
+        set => _alpha = Math.Clamp(value, 0, 255);
+    }
+
+    public GrayscaleColor()
+    {
+    }
+
+    public GrayscaleColor(double value, double alpha = 255)
+    {
+        Value = value;
+        Alpha = alpha;
+    }
+
+    public HsbColor ToHsb() => ColorConversion.GrayscaleToHsb(this);
+
+    public HslColor ToHsl() => ColorConversion.GrayscaleToHsl(this);
+    
+    public RgbColor ToRgb() => ColorConversion.GrayscaleToRgb(this);
+}
---
+++ src/DotNetCommons/Colors/HsbColor.cs
@@ -0,0 +1,52 @@
+﻿namespace DotNetCommons.Colors;
+
+public class HsbColor
+{
+    private double _hue;
+    private double _saturation;
+    private double _brightness;
+    private double _alpha;
+
+    public double Hue
+    {
+        get => _hue;
+        set
+        {
+            while (value < 0) value += 360;
+            while (value >= 360) value -= 360;
+            _hue = value;
+        }
+    }
+
+    public double Saturation
+    {
+        get => _saturation;
+        set => _saturation = Math.Clamp(value, 0, 100);
+    }
+
+    public double Brightness
+    {
+        get => _brightness;
+        set => _brightness = Math.Clamp(value, 0, 100);
+    }
+
+    public double Alpha
+    {
+        get => _alpha;
+        set => _alpha = Math.Clamp(value, 0, 255);
+    }
+
+    public HsbColor()
+    {
+    }
+
+    public HsbColor(double hue, double saturation, double brightness, double alpha = 255)
+    {
+        Hue        = hue;
+        Saturation = saturation;
+        Brightness = brightness;
+        Alpha      = alpha;
+    }
+
+    public RgbColor ToRgb() => ColorConversion.HsbToRgb(this);
+}
