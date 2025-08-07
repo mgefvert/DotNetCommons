d1031b759491289f796a945cb2a4bc3acde95922
Mats Gefvert <mats@gefvert.org>
2025-08-06 20:12:41 -0400

    Move Common extension methods into a separate folder

---
+++ src/DotNetCommons/_Extensions/CommonTimeProviderExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace DotNetCommons;
+
+public static class CommonTimeProviderExtensions
+{
+    public static DateTime Now(this TimeProvider clock) => clock.GetLocalNow().DateTime;
+    public static DateTime UtcNow(this TimeProvider clock) => clock.GetUtcNow().DateTime;
+    public static DateTime Today(this TimeProvider clock) => clock.GetLocalNow().Date;
+}
