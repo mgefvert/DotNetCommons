08891cba7052d4c3ff3fc9de6bebeebeb6adea9e
Mats Gefvert <mats@gefvert.org>
2026-03-20 16:17:15 -0400

    Add experimental TestDataReader class

---
+++ test/DotNetCommonTests.EF/TestData/Customer.cs
@@ -0,0 +1,22 @@
+﻿namespace DotNetCommonTests.EF.TestData;
+
+public enum CustomerType
+{
+    Individual,
+    Business,
+    Other
+}
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? CompanyName { get; set; }
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+    public string? Address { get; set; }
+    public string? City { get; set; }
+    public string? State { get; set; }
+    public string? Country { get; set; }
+}
