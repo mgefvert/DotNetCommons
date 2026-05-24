de4db391fd49d1fcec7eeacfce1d9f3b27525eb1
Mats Gefvert <mats@gefvert.org>
2026-05-24 18:43:07 -0400

    AppTagHelper content override

---
+++ src/DotNetCommons.Web/Html/DebugJson.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Html;
+
+namespace DotNetCommons.Web.Html;
+
+public static class DebugJson
+{
+    private static readonly JsonSerializerOptions SerializerOptions = new()
+    {
+        ReferenceHandler = ReferenceHandler.IgnoreCycles,
+        WriteIndented = true
+    };
+
+    public static HtmlString Serialize(object obj)
+    {
+        return new HtmlString("<pre style=\"text-align: left\">" + JsonSerializer.Serialize(obj, SerializerOptions) + "</pre>");
+    }
+}
