9f2d43ae13c2717dc23d91e6c29addb8235bdb7c
mgefvert <mgefvert@healthoptions.org>
2026-06-15 22:05:07 -0400

    Add CssClassList class

---
+++ src/DotNetCommons/Collections/CssClassList.cs
@@ -0,0 +1,119 @@
+﻿namespace DotNetCommons.Collections;
+
+public class CssClassList
+{
+    private readonly HashSet<string> _classes = [];
+
+    public IEnumerable<string> Items => _classes.Order();
+
+    public string Text
+    {
+        get => string.Join(" ", _classes.Order());
+        set => Set(value);
+    }
+
+    public CssClassList()
+    {
+    }
+
+    public CssClassList(string classes)
+    {
+        Add(classes);
+    }
+
+    public CssClassList(IEnumerable<string> classes)
+    {
+        Add(classes);
+    }
+
+    private string[] MakeList(string? classes) => classes.IsEmpty() ? [] : classes.Split(' ', StringSplitOptions.RemoveEmptyEntries).Distinct().ToArray();
+
+    private string[] MakeList(IEnumerable<string>? classes) => classes?.SelectMany(MakeList).Distinct().ToArray() ?? [];
+
+    public CssClassList Add(string? classes)
+    {
+        foreach (var c in MakeList(classes))
+            _classes.Add(c);
+        return this;
+    }
+
+    public CssClassList Add(IEnumerable<string> classes)
+    {
+        foreach (var c in MakeList(classes))
+            _classes.Add(c);
+        return this;
+    }
+
+    public CssClassList Clear()
+    {
+        _classes.Clear();
+        return this;
+    }
+
+    public bool Contains(string? classes)
+    {
+        return !classes.IsEmpty() && _classes.IsSupersetOf(MakeList(classes));
+    }
+
+    public bool Contains(IEnumerable<string> classes)
+    {
+        var list = MakeList(classes);
+        return list.IsAtLeastOne() && _classes.IsSupersetOf(list);
+    }
+
+    public CssClassList Remove(string? classes)
+    {
+        foreach (var c in MakeList(classes))
+            _classes.Remove(c);
+        return this;
+    }
+
+    public CssClassList Remove(IEnumerable<string> classes)
+    {
+        foreach (var c in MakeList(classes))
+            _classes.Remove(c);
+        return this;
+    }
+
+    public CssClassList Set(string? classes)
+    {
+        Clear();
+        Add(classes);
+        return this;
+    }
+
+    public CssClassList Set(IEnumerable<string> classes)
+    {
+        Clear();
+        Add(classes);
+        return this;
+    }
+
+    public CssClassList Toggle(string? classes)
+    {
+        var list = MakeList(classes);
+        var (removes, adds) = list.Toss(_classes.Contains);
+
+        foreach (var c in adds)
+            _classes.Add(c);
+        foreach (var c in removes)
+            _classes.Remove(c);
+
+        return this;
+    }
+
+    public CssClassList Toggle(IEnumerable<string> classes)
+    {
+        var list = MakeList(classes);
+        var (removes, adds) = list.Toss(_classes.Contains);
+
+        foreach (var c in adds)
+            _classes.Add(c);
+        foreach (var c in removes)
+            _classes.Remove(c);
+
+        return this;
+    }
+
+    public override string ToString() => Text;
+}
---
+++ test/DotNetCommonTests/Collections/CssClassList.cs
@@ -0,0 +1,72 @@
+﻿using DotNetCommons.Collections;
+
+namespace DotNetCommonTests.Collections;
+
+[TestClass]
+public class CssClassListTests
+{
+    [TestMethod]
+    public void Add_Works()
+    {
+        var classList = new CssClassList("btn");
+
+        classList.Add((string?)null);
+        Assert.AreEqual("btn", classList.Text);
+
+        classList.Add("btn");
+        Assert.AreEqual("btn", classList.Text);
+
+        classList.Add("btn  primary large  ");
+        Assert.AreEqual("btn large primary", classList.Text);
+    }
+
+    [TestMethod]
+    public void Remove_Works()
+    {
+        var classList = new CssClassList("btn primary xl");
+
+        classList.Add((string?)null);
+        Assert.AreEqual("btn primary xl", classList.Text);
+
+        classList.Remove("btn");
+        Assert.AreEqual("primary xl", classList.ToString());
+
+        classList.Remove(" a b c xl ");
+        Assert.AreEqual("primary", classList.ToString());
+    }
+
+    [TestMethod]
+    public void Clear_Works()
+    {
+        var classList = new CssClassList("btn primary");
+
+        classList.Clear();
+        Assert.AreEqual("", classList.Text);
+    }
+
+    [TestMethod]
+    public void Contains_Works()
+    {
+        var classList = new CssClassList("btn primary");
+
+        Assert.IsFalse(classList.Contains((string?)null));
+        Assert.IsFalse(classList.Contains(""));
+        Assert.IsTrue(classList.Contains("btn"));
+        Assert.IsTrue(classList.Contains(" primary "));
+        Assert.IsTrue(classList.Contains("primary btn"));
+        Assert.IsFalse(classList.Contains("xl"));
+    }
+
+    [TestMethod]
+    public void Toggle_Works()
+    {
+        var classList = new CssClassList("btn primary");
+        Assert.AreEqual("btn primary", classList.Text);
+
+        classList.Toggle("xl btn");
+        Assert.AreEqual("primary xl", classList.Text);
+
+        classList.Toggle("xl");
+        Assert.AreEqual("primary", classList.Text);
+    }
+}
