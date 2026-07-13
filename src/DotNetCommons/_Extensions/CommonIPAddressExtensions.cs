604bf8b325311136cc59d3f3b03abc929603c7ff
mgefvert <mgefvert@healthoptions.org>
2026-07-13 17:10:55 -0400

    Remove RDAP (duh), add IPAddress extensions

---
+++ src/DotNetCommons/_Extensions/CommonIPAddressExtensions.cs
@@ -0,0 +1,79 @@
+﻿using System.Buffers.Binary;
+using System.Net;
+using System.Net.Sockets;
+
+namespace DotNetCommons;
+
+public static class CommonIPAddressExtensions
+{
+    extension(IPAddress address)
+    {
+        public static IPAddress FromUInt32(uint value)
+        {
+            Span<byte> bytes = stackalloc byte[4];
+            BinaryPrimitives.WriteUInt32BigEndian(bytes, value);
+            return new IPAddress(bytes);
+        }
+
+        public static IPAddress FromUInt64(ulong value1, ulong value2)
+        {
+            Span<byte> bytes = stackalloc byte[16];
+            BinaryPrimitives.WriteUInt64BigEndian(bytes[..8], value1);
+            BinaryPrimitives.WriteUInt64BigEndian(bytes[8..], value2);
+            return new IPAddress(bytes);
+        }
+
+        public static IPAddress FromUInt128(UInt128 value)
+        {
+            Span<byte> bytes = stackalloc byte[16];
+            BinaryPrimitives.WriteUInt128BigEndian(bytes, value);
+            return new IPAddress(bytes);
+        }
+
+        public bool ToUInt32(out uint a)
+        {
+            a = 0;
+            if (address.AddressFamily != AddressFamily.InterNetwork)
+                return false;
+
+            Span<byte> bytes = stackalloc byte[4];
+            address.TryWriteBytes(bytes, out _);
+
+            a = BinaryPrimitives.ReadUInt32BigEndian(bytes);
+            return true;
+        }
+
+        public bool ToUInt64(out ulong a1, out ulong a2)
+        {
+            a1 = a2 = 0;
+
+            if (address.AddressFamily == AddressFamily.InterNetwork)
+                address = address.MapToIPv6();
+            if (address.AddressFamily != AddressFamily.InterNetworkV6)
+                return false;
+
+            Span<byte> bytes = stackalloc byte[16];
+            address.TryWriteBytes(bytes, out _);
+
+            a1 = BinaryPrimitives.ReadUInt64BigEndian(bytes[..8]);
+            a2 = BinaryPrimitives.ReadUInt64BigEndian(bytes[8..]);
+            return true;
+        }
+
+        public bool ToUInt128(out UInt128 a)
+        {
+            a = 0;
+
+            if (address.AddressFamily == AddressFamily.InterNetwork)
+                address = address.MapToIPv6();
+            if (address.AddressFamily != AddressFamily.InterNetworkV6)
+                return false;
+
+            Span<byte> bytes = stackalloc byte[16];
+            address.TryWriteBytes(bytes, out _);
+
+            a = BinaryPrimitives.ReadUInt128BigEndian(bytes);
+            return true;
+        }
+    }
+}
---
+++ test/DotNetCommonTests/_Extensions/CommonIPAddressExtensionsTest.cs
@@ -0,0 +1,77 @@
+﻿using System.Globalization;
+using System.Net;
+using DotNetCommons;
+using FluentAssertions;
+
+namespace DotNetCommonTests;
+
+// Condition is always known to be true
+#pragma warning disable MSTEST0032
+
+[TestClass]
+public class CommonIPAddressExtensionsTest
+{
+    [TestMethod]
+    public void TestUInt32_IpV4()
+    {
+        var ip = IPAddress.Parse("1.2.128.0");
+        ip.ToUInt32(out var a).Should().BeTrue();
+        a.Should().Be(0x01028000);
+
+        var ip2 = IPAddress.FromUInt32(a);
+        ip2.Should().Be(ip);
+    }
+
+    [TestMethod]
+    public void TestUInt32_IpV6()
+    {
+        var ip = IPAddress.Parse("2001:218:4000:6::");
+        ip.ToUInt32(out var a).Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void TestUInt64_IpV4()
+    {
+        var ip = IPAddress.Parse("1.2.128.0");
+        ip.ToUInt64(out var a1, out var a2).Should().BeTrue();
+        a1.Should().Be(0ul);
+        a2.Should().Be(0x0000FFFF_01028000ul);
+
+        var ip2 = IPAddress.FromUInt64(a1, a2);
+        ip2.Should().Be(ip.MapToIPv6());
+    }
+
+    [TestMethod]
+    public void TestUInt64_IpV6()
+    {
+        var ip = IPAddress.Parse("2001:218:4000:6::");
+        ip.ToUInt64(out var a1, out var a2).Should().BeTrue();
+        a1.Should().Be(0x20010218_40000006ul);
+        a2.Should().Be(0ul);
+
+        var ip2 = IPAddress.FromUInt64(a1, a2);
+        ip2.Should().Be(ip);
+    }
+
+    [TestMethod]
+    public void TestUInt128_IpV4()
+    {
+        var ip = IPAddress.Parse("1.2.128.0");
+        ip.ToUInt128(out var a).Should().BeTrue();
+        a.ToString("X32").Should().Be("00000000000000000000FFFF01028000");
+
+        var ip2 = IPAddress.FromUInt128(a);
+        ip2.Should().Be(ip.MapToIPv6());
+    }
+
+    [TestMethod]
+    public void TestUInt128_IpV6()
+    {
+        var ip = IPAddress.Parse("2001:218:4000:6::");
+        ip.ToUInt128(out var a).Should().BeTrue();
+        a.ToString("X32").Should().Be("20010218400000060000000000000000");
+
+        var ip2 = IPAddress.FromUInt128(a);
+        ip2.Should().Be(ip);
+    }
+}
