a6807f2350a8d0a5bbda44150fc759c77a6f91ee
mgefvert <mgefvert@healthoptions.org>
2026-08-28 12:29:12 -0400

    Add hard margins and micro-adjustments for label printing

---
+++ src/DotNetCommons/_Extensions/CommonDrawingExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+
+namespace DotNetCommons;
+
+public static class CommonDrawingExtensions
+{
+    public static void Invert(this PointF point)
+    {
+        point.X = -point.X;
+        point.Y = -point.Y;
+    }
+
+    public static void Scale(this RectangleF rect, float scaleX, float scaleY)
+    {
+        rect.Width  *= scaleX;
+        rect.Height *= scaleY;
+    }
+
+    public static void ScaleToMin(this RectangleF rect, SizeF minSize)
+    {
+        var scaleX = minSize.Width / rect.Width;
+        var scaleY = minSize.Height / rect.Height;
+        rect.Scale(Math.Max(scaleX, scaleY), Math.Max(scaleX, scaleY));
+    }
+
+    public static void ScaleToMax(this RectangleF rect, SizeF maxSize)
+    {
+        var scaleX = maxSize.Width / rect.Width;
+        var scaleY = maxSize.Height / rect.Height;
+        rect.Scale(Math.Min(scaleX, scaleY), Math.Min(scaleX, scaleY));
+    }
+}
